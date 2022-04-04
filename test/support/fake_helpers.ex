518734b7309644e92ff603b1e8e21f59bae253c1
Mayel de Borniol <git@mayel.space>
2022-04-04 15:24:37 +1200

    clean

--- test/support/fake_helpers.ex
+++ test/support/fake_helpers.ex
@@ -1,14 +1,5 @@
 defmodule Bonfire.Invite.Links.Test.FakeHelpers do
 
-  alias Bonfire.Data.Identity.Account
-  alias Bonfire.Invite.Links.Fake
-  alias Bonfire.Invite.Links.{Accounts, Users}
-  import ExUnit.Assertions
-
-  import Bonfire.Invite.Links.Integration
-
-  import Bonfire.Invite.Links.Fake
-
 
 
 end
