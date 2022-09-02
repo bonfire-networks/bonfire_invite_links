74584f8490432b577171fe187598ea26b10510af
Mayel de Borniol <git@mayel.space>
2022-09-02 14:49:51 +1200

    Switch to Untangle

--- lib/live_handler.ex
+++ lib/live_handler.ex
@@ -1,6 +1,6 @@
 defmodule Bonfire.Invite.Links.LiveHandler do
   use Bonfire.UI.Common.Web, :live_handler
-  import Where
+  import Untangle
 
   def handle_event("generate", %{"invite_link" => attrs}, socket) do
     # info(socket, "attrs")
