464c683c35bb45763eb1068939d5159ad9384665
Mayel de Borniol <git@mayel.space>
2022-11-24 21:34:19 +1300

    Persistence

--- lib/web/invites_live.ex
+++ lib/web/invites_live.ex
@@ -16,14 +16,19 @@ defmodule Bonfire.Invite.Links.Web.InvitesLive do
      )}
   end
 
-  def handle_event(action, attrs, socket),
-    do:
-      Bonfire.UI.Common.LiveHandlers.handle_event(
+  def handle_event(
         action,
         attrs,
-        socket,
-        __MODULE__
-      )
+        socket
+      ),
+      do:
+        Bonfire.UI.Common.LiveHandlers.handle_event(
+          action,
+          attrs,
+          socket,
+          __MODULE__
+          # &do_handle_event/3
+        )
 
   def handle_info(info, socket),
     do: Bonfire.UI.Common.LiveHandlers.handle_info(info, socket, __MODULE__)
