55ffefd23c2607d0bfec91541a00834f018663f7
Mayel de Borniol <git@mayel.space>
2024-02-06 18:55:43 +0000

    https://github.com/bonfire-networks/bonfire-app/issues/243

--- priv/repo/migrations/20200523081012_init_pointers.exs
+++ priv/repo/migrations/20200523081012_init_pointers.exs
@@ -1,4 +1,4 @@
-defmodule Bonfire.Invite.Links.Repo.Migrations.InitPointers  do
+defmodule Bonfire.Repo.Migrations.InitPointers do
   @moduledoc false
   use Ecto.Migration
   import Needle.Migration
